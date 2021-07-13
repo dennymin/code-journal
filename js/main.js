e3240c20be0ca677fec1c4ab5129f3512fa24f34
Denny Min <denden223@gmail.com>
2021-07-12 17:49:57 -0700

    Added the listen for 'submit' event to add new object, add nextEntryId to the object, increment Id, prepend, and reset other parts. #1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 /* global data */
 /* exported data */
-var $photoURL = document.querySelector('.photoURL');
+
+// photo url and preview photo code
+var $photoUrl = document.querySelector('.photoUrl');
 var $previewPhoto = document.querySelector('.previewPhoto');
 function updateSRC(event) {
-  $previewPhoto.src = $photoURL.value;
+  $previewPhoto.src = $photoUrl.value;
 }
 
-$photoURL.addEventListener('input', updateSRC);
+$photoUrl.addEventListener('input', updateSRC);
+
+// save button interactions code
+var $form = document.forms[0];
+var idCounter = 1;
+function retrieveData(event) {
+  event.preventDefault();
+  var dataEntry = {};
+  dataEntry.title = document.forms[0].elements.title.value;
+  dataEntry.pictureLink = document.forms[0].elements.photoUrl.value;
+  dataEntry.notes = document.forms[0].elements.notes.value;
+  data.entries.unshift(dataEntry);
+  data.nextEntryId = idCounter;
+  idCounter++;
+  $form.reset();
+  $previewPhoto.src = 'images/placeholder-image-square.jpg';
+}
+$form.addEventListener('submit', retrieveData);
