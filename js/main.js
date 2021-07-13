649197455051d0bf5135230c545033f64cf3b084
Denny Min <denden223@gmail.com>
2021-07-13 14:40:19 -0700

    Defined a function that takes in a journal entry and returns a DOM tree. Also, made edits to CSS and HTML to have more clearly defined class names. #2

--- js/main.js
+++ js/main.js
@@ -25,3 +25,33 @@ function retrieveData(event) {
   $previewPhoto.src = 'images/placeholder-image-square.jpg';
 }
 $form.addEventListener('submit', retrieveData);
+
+function createEntry(entry) {
+  var $newEntry = document.createElement('li');
+  $newEntry.setAttribute('class', 'entry-container');
+
+  var $newEntryImage = document.createElement('img');
+  $newEntryImage.setAttribute('class', 'column-half object-fit-contain object-position-start');
+  $newEntry.Image.setAttribute('src', entry.pictureLink);
+  $newEntry.appendChild($newEntryImage);
+
+  var $newEntryTextDiv = document.createElement('div');
+  $newEntryTextDiv.setAttribute('class', 'column-half entry-text');
+
+  var $newEntryTitle = document.createElement('h2');
+  var $newEntryTitleText = document.createTextNode(entry.title);
+  $newEntryTitle.setAttribute('class', 'entry-title-text');
+  $newEntryTitle.appendChild($newEntryTitleText);
+  $newEntryTextDiv.appendChild($newEntryTitle);
+
+  var $newEntryNotes = document.createElement('p');
+  var $newEntryNotesText = document.createTextNode(entry.notes);
+  $newEntryNotes.setAttribute('class', 'entry-notes-text');
+  $newEntryNotes.appendChild($newEntryNotesText);
+  $newEntryTextDiv.appendChild($newEntryNotes);
+
+  $newEntry.appendChild($newEntryTextDiv);
+  return $newEntry;
+}
+
+createEntry();
